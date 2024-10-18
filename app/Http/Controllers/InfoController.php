d62140a6850662c3d7c4d81486c5228dcf3e8bf3
Roxana <ms.roxanarahimi@gmail.com>
2024-10-18 23:05:59 +0330

    hi

--- app/Http/Controllers/InfoController.php
+++ app/Http/Controllers/InfoController.php
@@ -72,6 +72,6 @@ class InfoController extends Controller
 
     public function test()
     {
-        (new SaveWebPageTest('chrome'))->index();
+        (new SaveWebPageTest(''))->index();
     }
 }
